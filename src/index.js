c51d16a880b05678defc9a9296e2b8e39e16e98e
slevinto <slevinto@gmail.com>
2022-06-11 16:10:02 +0300

    My first commit

--- src/index.js
+++ src/index.js
@@ -53,12 +53,9 @@ app.post( '/', ( req, res ) => {
         
     try {
         const startTimestampUnix = req.body.sourceUpdate.startTimestampUnix
-        console.log( 'received startTimestampUnix\n', startTimestampUnix)
         const endTimestampUnix = req.body.sourceUpdate.endTimestampUnix
-        console.log( 'received endTimestampUnix\n', endTimestampUnix) 
         const authenticationToken = req.body.sourceUpdate.authenticationToken
-        console.log( 'received authenticationToken\n', authenticationToken) 
-        
+                
         data.startTimestampUnix = startTimestampUnix
         data.endTimestampUnix = endTimestampUnix
         data.authenticationToken = authenticationToken
@@ -70,15 +67,14 @@ app.post( '/', ( req, res ) => {
         data.authenticationToken = '2a94895e176b0116926cc95d011f1085'
         data.valueTypes = '1000,1200,3000'
     }
-        
+
+    console.log("send post: ", data)    
     axios.post(
         'https://api.und-gesund.de/v5/dynamicEpochValues',
         qs.stringify(data),
         config,
     ).then((res) => {
-        console.log("RESPONSE RECEIVED: ", res.data)
         res.data.forEach(dataSource => {
-            console.log("received dataSource: ", dataSource)
                 dataSource.dataSources.forEach(dataElem => {
                     console.log("received dataSource data: ", qs.parse(dataElem.data))
                     writeUserData(data.authenticationToken, qs.parse(dataElem.data))
