cf8302a599ffa1a07cef9201e28527cda114136e
slevinto <slevinto@gmail.com>
2022-07-16 15:05:16 +0300

    My first commit

--- src/route.js
+++ src/route.js
@@ -29,7 +29,7 @@ router.get('/', (req, res) => {
         allUsers.push(attributename);
       } 
       console.log("users: " + allUsers) 
-      res.sendFile("views/index.html", { root: '.' });
+      res.sendFile("/src/views/index.html", { root: '.' });
     } else {
     console.log("No data available")    
     }
