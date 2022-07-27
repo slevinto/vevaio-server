7d0c17b4d1899857737477fb85cca2d155ff5215
slevinto <slevinto@gmail.com>
2022-07-27 21:00:02 +0300

    My first commit

--- src/index.js
+++ src/index.js
@@ -187,7 +187,7 @@ app.post('/login_doctor', (req, res)=>{
     })
     .catch((error) => {
       // Handle Errors here.  
-      alert('failed to login in firebase: ' + error.message, 'kdialog')
+      alert('failed to login in firebase: ' + error.message, 'dialog')
       res.render('login', {credentials: {email: '', password: ''}} )
   })
 }) 
